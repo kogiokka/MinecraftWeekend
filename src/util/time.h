8c796fec5befcf7e141de767e87c4c73cc083978
Luis Kao <kogiokka.frms@gmail.com>
2022-01-01 01:16:23 +0800

    Make the code able to compiled by MinGW on Windows

--- src/util/time.h
+++ src/util/time.h
@@ -1,14 +1,11 @@
 #ifndef TIME_H
 #define TIME_H
 
-#include <time.h>
+#include "../gfx/gfx.h"
 
 #define NS_PER_SECOND (1000000000)
 #define NS_PER_MS (1000000)
 
-#define NOW() ({\
-    struct timespec ts;\
-    timespec_get(&ts, TIME_UTC);\
-    ((ts.tv_sec * NS_PER_SECOND) + ts.tv_nsec);})
+#define NOW() glfwGetTime() * NS_PER_SECOND
 
 #endif
