df1a8fe11019136385c3e0ff209926a04bf53a1c
Luis Kao <kogiokka.frms@gmail.com>
2022-01-07 18:10:24 +0800

    Add tag/version to FetchContent targets

--- cmake/FetchCGLM.cmake
+++ cmake/FetchCGLM.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 set(cglm_SOURCE_DIR "${CMAKE_SOURCE_DIR}/external/cglm")
 
 FetchContent_Declare(
-    cglm
+    cglm-v0.8.4
     GIT_REPOSITORY "https://github.com/recp/cglm.git"
     GIT_TAG "34e5704fe88da1e277ef4ab6c498eacfa464a391" # v0.8.4
     SOURCE_DIR "${cglm_SOURCE_DIR}"
@@ -12,9 +12,9 @@ FetchContent_Declare(
 
 set(FETCHCONTENT_QUIET FALSE)
 if (EXISTS ${cglm_SOURCE_DIR})
-    set(FETCHCONTENT_SOURCE_DIR_CGLM ${cglm_SOURCE_DIR})
+    set(FETCHCONTENT_SOURCE_DIR_CGLM-V0.8.4 ${cglm_SOURCE_DIR})
 endif()
 
 option(CGLM_STATIC "CGLM static build" ON)
 option(CGLM_SHARED "CGLM shared build" OFF)
-FetchContent_MakeAvailable(cglm)
+FetchContent_MakeAvailable(cglm-v0.8.4)
--- cmake/FetchGLFW.cmake
+++ cmake/FetchGLFW.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 set(glfw_SOURCE_DIR "${CMAKE_SOURCE_DIR}/external/glfw")
 
 FetchContent_Declare(
-    glfw
+    glfw-3.3.6
     GIT_REPOSITORY "https://github.com/glfw/glfw.git"
     GIT_TAG "7d5a16ce714f0b5f4efa3262de22e4d948851525" # tag 3.3.6
     SOURCE_DIR "${glfw_SOURCE_DIR}"
@@ -12,11 +12,11 @@ FetchContent_Declare(
 
 set(FETCHCONTENT_QUIET FALSE)
 if (EXISTS ${glfw_SOURCE_DIR})
-    set(FETCHCONTENT_SOURCE_DIR_GLFW ${glfw_SOURCE_DIR})
+    set(FETCHCONTENT_SOURCE_DIR_GLFW-3.3.6 "${glfw_SOURCE_DIR}")
 endif()
 
 option(GLFW_BUILD_DOCS "GLFW documentation" OFF)
 option(GLFW_BUILD_EXAMPLES "GLFW examples" OFF)
 option(GLFW_BUILD_TESTS "GLFW tests" OFF)
 option(GLFW_BUILD_INSTALL "GLFW installation target" OFF)
-FetchContent_MakeAvailable(glfw)
+FetchContent_MakeAvailable(glfw-3.3.6)
